7ded10c658788c2f7ea59c1a9b7bfb2a803f0f32
Type-Here <94553428+Type-Here@users.noreply.github.com>
2024-11-22 17:02:33 +0100

    Added AppBar in login test page

--- src/lib/testing_page.dart
+++ src/lib/testing_page.dart
@@ -8,6 +8,9 @@ class TestingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Testing Page'),
+      ),
       body: Text(user.email),
     );
   }
