14811910fc66a93816a61ceea9784ebdc99c1fc5
Type-Here <94553428+Type-Here@users.noreply.github.com>
2024-11-27 03:07:16 +0100

    logo_widget.dart: now widget use logo_blu-cropped.svg. Improved style. Added opt borderRadius construct param. Added Comments

--- src/lib/widgets/logo_widget.dart
+++ src/lib/widgets/logo_widget.dart
@@ -5,29 +5,36 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 /// A custom widget that displays the application's logo.
+/// This widget is designed to show the logo of the application.
+/// The logo is displayed as an SVG image.
+/// The widget is customizable optional with a `borderRadius` parameter.
 class LogoWidget extends StatelessWidget {
+
+  /// The border radius of the logo widget
+  /// Default value is 0.0
+  /// If set, the logo will be clipped with the circular specified border radius.
+  final double borderRadius;
+
   /// Creates a custom logo widget.
-  const LogoWidget({super.key});
+  const LogoWidget({super.key, this.borderRadius = 10.0});
 
   @override
   Widget build(BuildContext context) {
     String logoPath = kIsWeb || !Platform.isAndroid
-        ? 'images/logo_blu.svg'
-        : 'assets/images/logo_blu.svg';
+        ? 'images/logo_blu-cropped.svg'
+        : 'assets/images/logo_blu-cropped.svg';
 
     return ClipRRect(
-      borderRadius: BorderRadius.circular(125.0),
+      borderRadius: BorderRadius.circular(borderRadius),
       clipBehavior: Clip.antiAliasWithSaveLayer,
-      child: Container(
-        child: SvgPicture.asset(
-          logoPath,
-          fit: BoxFit.cover,
-          height: 200,
-          width: 200,
-          semanticsLabel: 'Logo CiviConnect',
-          placeholderBuilder: (context) =>
-              const CircularProgressIndicator(backgroundColor: Colors.blue),
-        ),
+      child: SvgPicture.asset(
+        logoPath,
+        fit: BoxFit.cover,
+        height: MediaQuery.of(context).size.height * 0.2,
+        //width: 200,
+        semanticsLabel: 'Logo CiviConnect',
+        placeholderBuilder: (context) =>
+            const CircularProgressIndicator(backgroundColor: Colors.blue),
       ),
     );
   }
