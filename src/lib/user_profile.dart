7e6447da5ad7dbc3877cf3b9ba5c962d07208c8b
FaiellaFrancesco <faiellafrancesco2003@gmail.com>
2024-11-28 10:51:37 +0100

    Aggiunti dettagli areaUtente: migliorato lo spacing, aggiustati i font, aggiunta icona di default. Ora la GUI prendi i dati dal db. TODO: Bisognerebbe capire se è un cittadino o un comune e comportarsi di conseguenza
    Co-authored-by: Generoso Sorridi <alessiosorridi@hotmail.it>

--- src/lib/user_profile.dart
+++ src/lib/user_profile.dart
@@ -1,80 +1,74 @@
 import 'package:civiconnect/theme.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'firebase_options.dart';
 class UserProfile extends StatelessWidget {
-  UserProfile({Key? key}) : super(key: key);
+  UserProfile({super.key});
 
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = ThemeManager().customTheme;
     final user = FirebaseAuth.instance.currentUser;
+    String uid = user!.uid;
+    /* test
     if (user == null) {
       print("L'utente non è autenticato");
     } else {
-      print("Utente autenticato: ${user.email}");
-    }
+      print('Utente autenticato: ${user.email}');
+    }*/
 
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Area Utente",
+          'Area Utente',
           style: theme.textTheme.titleLarge,
         ),
+        centerTitle: true,
       ),
-      body: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
+      body: FutureBuilder<DocumentSnapshot>(
+        future: FirebaseFirestore.instance.collection('citizen').doc(uid).get(),
         builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) { //controlliamo stato dell utente
-            return Center(child: CircularProgressIndicator());
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
-            return Center(child: Text("Something went wrong: ${snapshot.error}"));
-          } else if (!snapshot.hasData) {
-            return Center(child: Text("User is not signed in"));
+            return Center(
+              child: Text(
+                'Something went wrong: ${snapshot.error}',
+                style: theme.textTheme.titleMedium,
+              ),
+            );
           } else {
-            final user = snapshot.data!;
+            final userData = snapshot.data!.data() as Map<String, dynamic>;
+
             return SingleChildScrollView(
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
+                    const SizedBox(height: 0),
+                    _buildProfileHeader(theme, user, userData),
+                    const SizedBox(height: 30),
+                    Text(
+                      'Dati Personali',
+                      style: theme.textTheme.titleLarge?.copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 10),
+                    ..._buildPersonalData(userData, theme),
                     const SizedBox(height: 20),
-                    Center(
-                      child: CircleAvatar(
-                        radius: 100,
-                        backgroundImage: user.photoURL != null
-                            ? NetworkImage(user.photoURL!)
-                            : AssetImage('src/web/favicon.png') as ImageProvider, //Qui andrebbe messa un img di base, in caso di error mostra quella
-                      ),
+                    const Divider(
+                      thickness: 1, // Spessore della linea
+                      height: 10,   // Altezza della linea e spazio intorno
+                      color: Color.fromRGBO(0, 69, 118, 1), // Colore della linea
                     ),
-                    Center(
-                      child: Text(
-                        "Bentornato, ${user.displayName ?? 'User'}!",
-                        style: theme.textTheme.titleLarge,
-                      ),
+                    const SizedBox(height: 20),
+                    Text(
+                      'Dati Account',
+                      style: theme.textTheme.titleLarge?.copyWith(fontWeight: FontWeight.bold),
                     ),
                     const SizedBox(height: 10),
-                    Text(
-                      "Dati Personali",
-                      style: theme.textTheme.titleLarge,
-                    ),
-                    const SizedBox(height: 10),
-                    Text(
-                      "Nome: ${user.displayName ?? 'N/A'}",
-                      style: theme.textTheme.titleMedium,
-                    ),
-                    Text(
-                      "E-Mail: ${user.email ?? 'N/A'}",
-                      style: theme.textTheme.titleMedium,
-                    ),
-                    Text(
-                      "Indirizzo:}",
-                      style: theme.textTheme.titleMedium,
-
-                    ),
+                    _buildAccountData(user, theme),
                   ],
                 ),
               ),
@@ -85,7 +79,7 @@ class UserProfile extends StatelessWidget {
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.person),
+            icon: Icon(Icons.assignment),
             label: 'Segnalazioni',
           ),
           BottomNavigationBarItem(
@@ -98,6 +92,121 @@ class UserProfile extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+
+  // Widget per l'header con immagine e benvenuto
+  Widget _buildProfileHeader(
+      ThemeData theme, User user, Map<String, dynamic> userData) {
+    return Center(
+      child: Column(
+        children: [
+          CircleAvatar(
+            radius: 80,
+            backgroundImage: user.photoURL != null
+                ? NetworkImage(user.photoURL!)
+                : null,
+            child: user.photoURL == null
+                ? Icon(
+              Icons.person,
+              size: 80,
+            )
+                : null,
+          ),
+          const SizedBox(height: 5),
+          Text(
+            "Bentornato, ${userData['firstName'] ?? 'User'}!",
+            style: theme.textTheme.titleLarge,
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Funzione per costruire i dati personali
+  List<Widget> _buildPersonalData(Map<String, dynamic> userData, ThemeData theme) {
+    final List<Map<String, String>> personalFields = [
+      {'Nome': userData['firstName'] ?? 'N/A'},
+      {'Cognome': userData['lastName'] ?? 'N/A'},
+      {'Indirizzo': userData['address']['street'] ?? 'N/A'},
+      {'Città': userData['city'] ?? 'N/A'},
+      {'CAP': userData['CAP'] ?? 'N/A'},
+    ];
+
+    return personalFields
+        .map(
+          (field) => Padding(
+        padding: const EdgeInsets.symmetric(vertical: 3.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              flex: 2,
+              child: Text(
+                '${field.keys.first}:',
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 13,
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Text(
+                field.values.first,
+                style: theme.textTheme.titleMedium,
+              ),
+            ),
+          ],
+        ),
+      ),
+    )
+        .toList();
+  }
+
+  // Widget per i dati dell'account
+  Widget _buildAccountData(User user, ThemeData theme) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Text(
+                'Email:',
+                style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Text(
+                user.email ?? 'N/A',
+                style: theme.textTheme.titleMedium,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 10),
+        Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Text(
+                'Password:',
+                style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Text(
+                '********', // Puoi gestire la modifica della password separatamente
+                style: theme.textTheme.titleMedium,
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
