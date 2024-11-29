441855ede41796b8cc7aff108dd828177a4d75fa
FaiellaFrancesco <faiellafrancesco2003@gmail.com>
2024-11-29 23:32:11 +0100

    fixed CitizenArea, added Email and Password
    Co-authored-by: Generoso Sorridi <alessiosorridi@hotmail.it>

--- src/lib/user_profile.dart
+++ src/lib/user_profile.dart
@@ -69,6 +69,8 @@ class CitizenArea extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -99,43 +101,80 @@ class CitizenArea extends StatelessWidget {
           }
 
           final userData = snapshot.data!.data() as Map<String, dynamic>;
-          return _buildContent(userData);
+          return _buildContent(userData, theme);
         },
       ),
     );
   }
 
-  Widget _buildContent(Map<String, dynamic> userData) {
+  Widget _buildContent(Map<String, dynamic> userData, ThemeData theme) {
     return SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const SizedBox(height: 0),
+            // Avatar centrato
+            Center(
+              child: CircleAvatar(
+                radius: 80,
+                child: Icon(Icons.person, size: 80),
+              ),
+            ),
+            const SizedBox(height: 20),
+            // Sezione Dati Personali
             Text(
-              'Benvenuto, ${userData['firstName'] ?? 'Cittadino'}!',
+              'Dati Personali',
+              style: theme.textTheme.titleLarge,
+            ),
+            const SizedBox(height: 10),
+            ..._buildPersonalData(userData, theme),
+            const SizedBox(height: 20),
+            const Divider(
+              thickness: 1,
+              color: Color.fromRGBO(0, 69, 118, 1),
+            ),
+            const SizedBox(height: 20),
+            // Sezione Dati Account
+            Text(
+              'Dati Account',
               style: theme.textTheme.titleLarge,
             ),
             const SizedBox(height: 20),
-            ..._buildPersonalData(userData),
+           text_widget(boldText: 'Email', normalText: userData['email'] ?? 'N/A'),
+            const SizedBox(height: 10),
+            text_widget(boldText: 'Password', normalText: '******'),
           ],
         ),
       ),
     );
   }
 
-  List<Widget> _buildPersonalData(Map<String, dynamic> userData) {
+  List<Widget> _buildPersonalData(Map<String, dynamic> userData, ThemeData theme) {
     return [
-      Text('Nome: ${userData['firstName'] ?? 'N/A'}'),
-      Text('Cognome: ${userData['lastName'] ?? 'N/A'}'),
-      Text('Indirizzo: ${userData['address']?['street'] ?? 'N/A'}'),
-      Text('Città: ${userData['city'] ?? 'N/A'}'),
-      Text('CAP: ${userData['CAP'] ?? 'N/A'}'),
+      Text(
+        'Nome: ${userData['firstName'] ?? 'N/A'}',
+        style: theme.textTheme.titleMedium,
+      ),
+      Text(
+        'Cognome: ${userData['lastName'] ?? 'N/A'}',
+        style: theme.textTheme.titleMedium,
+      ),
+      Text(
+        'Indirizzo: ${userData['address']?['street'] ?? 'N/A'}',
+        style: theme.textTheme.titleMedium,
+      ),
+      Text(
+        'Città: ${userData['city'] ?? 'N/A'}',
+        style: theme.textTheme.titleMedium,
+      ),
+      Text(
+        'CAP: ${userData['CAP'] ?? 'N/A'}',
+        style: theme.textTheme.titleMedium,
+      ),
     ];
   }
 }
-
 class MunicipalityArea extends StatelessWidget {
   final String uid;
 
@@ -179,6 +218,7 @@ class MunicipalityArea extends StatelessWidget {
     );
   }
 
+
   Widget _buildContent(Map<String, dynamic> municipalityData) {
     return SingleChildScrollView(
       child: Padding(
@@ -188,27 +228,30 @@ class MunicipalityArea extends StatelessWidget {
           children: [
             const SizedBox(height: 0),
             Text(
-              'Benvenuto, Comune di ${municipalityData['name'] ?? 'Municipality'}!',
+              'Benvenuto, Comune di ${municipalityData['municipalityName'] ?? 'N/A'}!',
               style: theme.textTheme.titleLarge,
             ),
             const SizedBox(height: 20),
 
             // Nome con grassetto e spaziatura
+            /*
             Text(
               'Nome: ',
-              style: theme.textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.bold),
+              style: theme.textTheme.titleLarge?.copyWith(fontWeight: FontWeight.bold),
             ),
             Text('${municipalityData['municipalityName'] ?? 'N/A'}'),
             const SizedBox(height: 10), // Aggiungi spazio tra Nome e Email
-
+            */
             // Email con grassetto e spaziatura
+            /*
             Text(
               'Email: ',
-              style: theme.textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.bold),
+              style: theme.textTheme.titleLarge?.copyWith(fontWeight: FontWeight.bold),
             ),
             Text('${municipalityData['email'] ?? 'N/A'}'),
             const SizedBox(height: 10), // Aggiungi spazio tra Email e Provincia
-
+            */
+            text_widget(boldText: "Email:", normalText: '${municipalityData['email'] ?? 'N/A'}'),
             // Provincia con grassetto e spaziatura
             Text(
               'Provincia: ',
@@ -219,5 +262,44 @@ class MunicipalityArea extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+
+class text_widget extends StatelessWidget {
+  final String boldText;
+  final String normalText;
+  
+
+  text_widget({super.key, required this.boldText,required this.normalText});
+
+  @override
+  Widget build(BuildContext context) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      //first column
+      Expanded(
+          flex:2,
+          child: Text(
+
+        boldText,
+        style: const TextStyle(fontWeight: FontWeight.bold),
+
+      )),
+
+      //second column
+      Expanded(
+          flex:2,
+          child: Text(
+        normalText,
+            style: const TextStyle(fontSize: 13),
+
+      )),
+    ],
+
+
+  );
+
   }
 }
