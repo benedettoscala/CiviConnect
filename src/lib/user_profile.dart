d132b461b23b8dd7c2751428edcd3ba83e3284a5
FaiellaFrancesco <faiellafrancesco2003@gmail.com>
2024-11-29 21:53:02 +0100

    added class CitizenArea and MunicipalityArea
    Co-authored-by: Generoso Sorridi <alessiosorridi@hotmail.it>

--- src/lib/user_profile.dart
+++ src/lib/user_profile.dart
@@ -3,25 +3,76 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+final ThemeData theme = ThemeManager().customTheme;
+
 class UserProfile extends StatelessWidget {
   UserProfile({super.key});
 
+  final user = FirebaseAuth.instance.currentUser;
+
+  Future<String> _getUserType(String uid) async {
+    final citizenDoc = await FirebaseFirestore.instance.collection('citizen').doc(uid).get();
+    if (citizenDoc.exists) return 'citizen';
+
+    final municipalityDoc = await FirebaseFirestore.instance.collection('municipality').doc(uid).get();
+    if (municipalityDoc.exists) return 'municipality';
+
+    return 'unknown';
+  }
+
   @override
   Widget build(BuildContext context) {
-    final ThemeData theme = ThemeManager().customTheme;
-    final user = FirebaseAuth.instance.currentUser;
     String uid = user!.uid;
-    /* test
-    if (user == null) {
-      print("L'utente non è autenticato");
-    } else {
-      print('Utente autenticato: ${user.email}');
-    }*/
 
+    return FutureBuilder<String>(
+      future: _getUserType(uid),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        } else if (snapshot.hasError || snapshot.data == 'unknown') {
+          return Scaffold(
+            body: Center(
+              child: Text(
+                'Errore nel caricamento dei dati utente.',
+                style: theme.textTheme.titleMedium,
+              ),
+            ),
+          );
+        }
+
+        final userType = snapshot.data!;
+        if (userType == 'citizen') {
+          return CitizenArea(uid: uid);
+        } else if (userType == 'municipality') {
+          return MunicipalityArea(uid: uid);
+        } else {
+          return Scaffold(
+            body: Center(
+              child: Text(
+                'Tipo utente non riconosciuto.',
+                style: theme.textTheme.titleMedium,
+              ),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
+
+class CitizenArea extends StatelessWidget {
+  final String uid;
+
+  CitizenArea({super.key, required this.uid});
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Area Utente',
+          'Area Cittadino',
           style: theme.textTheme.titleLarge,
         ),
         centerTitle: true,
@@ -34,179 +85,139 @@ class UserProfile extends StatelessWidget {
           } else if (snapshot.hasError) {
             return Center(
               child: Text(
-                'Something went wrong: ${snapshot.error}',
+                'Errore nel caricamento dei dati: ${snapshot.error}',
                 style: theme.textTheme.titleMedium,
               ),
             );
-          } else {
-            final userData = snapshot.data!.data() as Map<String, dynamic>;
-
-            return SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 0),
-                    _buildProfileHeader(theme, user, userData),
-                    const SizedBox(height: 30),
-                    Text(
-                      'Dati Personali',
-                      style: theme.textTheme.titleLarge?.copyWith(fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 10),
-                    ..._buildPersonalData(userData, theme),
-                    const SizedBox(height: 20),
-                    const Divider(
-                      thickness: 1, // Spessore della linea
-                      height: 10,   // Altezza della linea e spazio intorno
-                      color: Color.fromRGBO(0, 69, 118, 1), // Colore della linea
-                    ),
-                    const SizedBox(height: 20),
-                    Text(
-                      'Dati Account',
-                      style: theme.textTheme.titleLarge?.copyWith(fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 10),
-                    _buildAccountData(user, theme),
-                  ],
-                ),
+          } else if (!snapshot.hasData || !snapshot.data!.exists) {
+            return Center(
+              child: Text(
+                'Dati utente non trovati.',
+                style: theme.textTheme.titleMedium,
               ),
             );
           }
+
+          final userData = snapshot.data!.data() as Map<String, dynamic>;
+          return _buildContent(userData);
         },
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.assignment),
-            label: 'Segnalazioni',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profilo',
-          ),
-        ],
-      ),
     );
   }
 
-  // Widget per l'header con immagine e benvenuto
-  Widget _buildProfileHeader(
-      ThemeData theme, User user, Map<String, dynamic> userData) {
-    return Center(
-      child: Column(
-        children: [
-          CircleAvatar(
-            radius: 80,
-            backgroundImage: user.photoURL != null
-                ? NetworkImage(user.photoURL!)
-                : null,
-            child: user.photoURL == null
-                ? Icon(
-              Icons.person,
-              size: 80,
-            )
-                : null,
-          ),
-          const SizedBox(height: 5),
-          Text(
-            "Bentornato, ${userData['firstName'] ?? 'User'}!",
-            style: theme.textTheme.titleLarge,
-          ),
-        ],
-      ),
-    );
-  }
-
-  // Funzione per costruire i dati personali
-  List<Widget> _buildPersonalData(Map<String, dynamic> userData, ThemeData theme) {
-    final List<Map<String, String>> personalFields = [
-      {'Nome': userData['firstName'] ?? 'N/A'},
-      {'Cognome': userData['lastName'] ?? 'N/A'},
-      {'Indirizzo': userData['address']['street'] ?? 'N/A'},
-      {'Città': userData['city'] ?? 'N/A'},
-      {'CAP': userData['CAP'] ?? 'N/A'},
-    ];
-
-    return personalFields
-        .map(
-          (field) => Padding(
-        padding: const EdgeInsets.symmetric(vertical: 3.0),
-        child: Row(
+  Widget _buildContent(Map<String, dynamic> userData) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Expanded(
-              flex: 2,
-              child: Text(
-                '${field.keys.first}:',
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 13,
-                ),
-              ),
+            const SizedBox(height: 0),
+            Text(
+              'Benvenuto, ${userData['firstName'] ?? 'Cittadino'}!',
+              style: theme.textTheme.titleLarge,
             ),
-            Expanded(
-              flex: 3,
-              child: Text(
-                field.values.first,
-                style: theme.textTheme.titleMedium,
-              ),
-            ),
+            const SizedBox(height: 20),
+            ..._buildPersonalData(userData),
           ],
         ),
       ),
-    )
-        .toList();
+    );
   }
 
-  // Widget per i dati dell'account
-  Widget _buildAccountData(User user, ThemeData theme) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          children: [
-            Expanded(
-              flex: 2,
+  List<Widget> _buildPersonalData(Map<String, dynamic> userData) {
+    return [
+      Text('Nome: ${userData['firstName'] ?? 'N/A'}'),
+      Text('Cognome: ${userData['lastName'] ?? 'N/A'}'),
+      Text('Indirizzo: ${userData['address']?['street'] ?? 'N/A'}'),
+      Text('Città: ${userData['city'] ?? 'N/A'}'),
+      Text('CAP: ${userData['CAP'] ?? 'N/A'}'),
+    ];
+  }
+}
+
+class MunicipalityArea extends StatelessWidget {
+  final String uid;
+
+  MunicipalityArea({super.key, required this.uid});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Area Comune',
+          style: theme.textTheme.titleLarge,
+        ),
+        centerTitle: true,
+      ),
+      body: FutureBuilder<DocumentSnapshot>(
+        future: FirebaseFirestore.instance.collection('municipality').doc(uid).get(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(
               child: Text(
-                'Email:',
-                style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
-              ),
-            ),
-            Expanded(
-              flex: 3,
-              child: Text(
-                user.email ?? 'N/A',
+                'Errore nel caricamento dei dati: ${snapshot.error}',
                 style: theme.textTheme.titleMedium,
               ),
-            ),
-          ],
-        ),
-        const SizedBox(height: 10),
-        Row(
-          children: [
-            Expanded(
-              flex: 2,
+            );
+          } else if (!snapshot.hasData || !snapshot.data!.exists) {
+            return Center(
               child: Text(
-                'Password:',
-                style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
-              ),
-            ),
-            Expanded(
-              flex: 3,
-              child: Text(
-                '********', // Puoi gestire la modifica della password separatamente
+                'Dati comune non trovati.',
                 style: theme.textTheme.titleMedium,
               ),
+            );
+          }
+
+          final municipalityData = snapshot.data!.data() as Map<String, dynamic>;
+          return _buildContent(municipalityData);
+        },
+      ),
+    );
+  }
+
+  Widget _buildContent(Map<String, dynamic> municipalityData) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 0),
+            Text(
+              'Benvenuto, Comune di ${municipalityData['name'] ?? 'Municipality'}!',
+              style: theme.textTheme.titleLarge,
             ),
+            const SizedBox(height: 20),
+
+            // Nome con grassetto e spaziatura
+            Text(
+              'Nome: ',
+              style: theme.textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.bold),
+            ),
+            Text('${municipalityData['municipalityName'] ?? 'N/A'}'),
+            const SizedBox(height: 10), // Aggiungi spazio tra Nome e Email
+
+            // Email con grassetto e spaziatura
+            Text(
+              'Email: ',
+              style: theme.textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.bold),
+            ),
+            Text('${municipalityData['email'] ?? 'N/A'}'),
+            const SizedBox(height: 10), // Aggiungi spazio tra Email e Provincia
+
+            // Provincia con grassetto e spaziatura
+            Text(
+              'Provincia: ',
+              style: theme.textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.bold),
+            ),
+            Text('${municipalityData['province'] ?? 'N/A'}'),
           ],
         ),
-      ],
+      ),
     );
   }
 }
