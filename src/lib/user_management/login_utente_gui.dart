18b98df53d486d9bfd3808481cb8f6990b09da5a
Type-Here <94553428+Type-Here@users.noreply.github.com>
2024-12-03 18:33:06 +0100

    login_utente_gui.dart and main.dart: modified loginUtenteGUI constructor to allow simplified controller injection

--- src/lib/user_management/login_utente_gui.dart
+++ src/lib/user_management/login_utente_gui.dart
@@ -18,13 +18,15 @@ import '../widgets/logo_widget.dart';
 /// A stateful widget to wrap the login form.
 class LoginUtenteGUI extends StatefulWidget {
   /// Constructs a new instance of [LoginUtenteGUI].
-  const LoginUtenteGUI({super.key});
+  LoginUtenteGUI({super.key, UserManagementController? controller})
+      : _controller = (controller == null) ? UserManagementController(redirectPage: HomePage()) : controller;
 
   /// The title of the login page.
   final String title = 'Login';
+  final UserManagementController? _controller;
 
   @override
-  State<LoginUtenteGUI> createState() => _LoginUtenteGUIState();
+  State<LoginUtenteGUI> createState() => _LoginUtenteGUIState(_controller);
 }
 
 class _LoginUtenteGUIState extends State<LoginUtenteGUI> {
@@ -35,6 +37,12 @@ class _LoginUtenteGUIState extends State<LoginUtenteGUI> {
   final _formKey = GlobalKey<FormBuilderState>();
   final _passKey = GlobalKey<FormBuilderFieldState>();
   final _emailKey = GlobalKey<FormBuilderFieldState>();
+  late final UserManagementController _controller;
+
+
+  _LoginUtenteGUIState(UserManagementController? controller) {
+    _controller = controller ?? UserManagementController(redirectPage: const HomePage());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -117,7 +125,7 @@ class _LoginUtenteGUIState extends State<LoginUtenteGUI> {
                                   focusNode: focusNode,
                                   validator: FormBuilderValidators.compose([
                                     FormBuilderValidators.password(
-                                        minLength: 6, maxLength: 255),
+                                        minLength: 8, maxLength: 255),
                                     FormBuilderValidators.required(),
                                   ]),
                                   obscureText: obscureText,
@@ -136,7 +144,8 @@ class _LoginUtenteGUIState extends State<LoginUtenteGUI> {
                                     });
                                   },
                                   onSubmitted: (value) {
-                                    _sendData(email, password);
+                                    // Sends the email and password to the controller.
+                                    _sendData(_controller, email, password);
                                   },
                                 ),
                               ],
@@ -153,7 +162,7 @@ class _LoginUtenteGUIState extends State<LoginUtenteGUI> {
                                   borderRadius: BorderRadius.circular(20),
                                 ),
                               ),
-                              onPressed: () => _sendData(email, password),
+                              onPressed: () => _sendData(_controller, email, password),
                               child: const Text(
                                 'Login',
                               ),
@@ -177,16 +186,13 @@ class _LoginUtenteGUIState extends State<LoginUtenteGUI> {
   /// Method to send the login data to the controller.
   /// This method validates the form and sends the email and password to the controller.
   /// If the user is not valid, a snackbar is displayed.
-  void _sendData(String email, String password) async {
+  void _sendData(UserManagementController controller, String email, String password) async {
     String motivation = 'Invalid email or password';
     final formState = _formKey.currentState;
     bool validUser = false;
     if (formState == null || !formState.saveAndValidate()) {
       return;
     }
-    // Sends the email and password to the controller.
-    UserManagementController controller =
-        UserManagementController(redirectPage: const HomePage());
     try {
       validUser =
       await controller.login(context, email: email, password: password);
