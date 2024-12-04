ee8783c627f2c6b5ae0769cb83acd2b8567abf93
Marco-Brescia <marcbresc03@gmail.com>
2024-12-04 20:23:55 +0100

    Added method to get reports of a Citizen

--- src/lib/gestione_segnalazione_cittadino/gestione_segnalazione_cittadino_controller.dart
+++ src/lib/gestione_segnalazione_cittadino/gestione_segnalazione_cittadino_controller.dart
@@ -1,3 +1,23 @@
-class CitizenReportManagementController{
+import '../model/users_model.dart';
+import 'gestione_segnalazione_cittadino_dao.dart';
 
+
+/// Controller for managing citizen reports.
+///
+/// This controller provides methods to interact with the data access object (DAO)
+/// for fetching and managing reports related to citizens.
+class CitizenReportManagementController{
+  final CitizenReportManagementDAO _reportDAO = CitizenReportManagementDAO();
+
+  /// Fetches the list of reports for a given citizen.
+  ///
+  /// This method retrieves the list of reports associated with the city of the provided citizen.
+  /// If the city is not available, it returns an empty list.
+  ///
+  /// \param user The [Citizen] object representing the user whose reports are to be fetched.
+  ///
+  /// \return A [Future] that resolves to a list of maps, where each map contains the report details.
+  Future<List<Map<String, dynamic>>> getUserReports(Citizen user) async {
+    return await _reportDAO.getReportList(user.city ?? '') ?? [];
+  }
 }
