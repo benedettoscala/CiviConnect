c6e90ae5d86fd17f5e84b77ff1c69c342432396f
GiuseppeGambardella <gagiuseppe29@gmail.com>
2024-12-04 11:42:45 +0100

    Inserimento Seganalazione GUI: aggiunta città alla GUI, stampa dinamica della posizione nei campi Città e Indirizzo, Refactoring codice
    Co-authored-by: FaiellaFrancesco <faiellafrancesco2003@gmail.com>

--- src/lib/gestione_segnalazione/inserimento_segnalazione_gui.dart
+++ src/lib/gestione_segnalazione/inserimento_segnalazione_gui.dart
@@ -1,29 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:civiconnect/widgets/input_textfield_decoration.dart';
-import 'package:location/location.dart'as loc;
+import 'package:location/location.dart' as loc;
 import 'package:geocoding/geocoding.dart';
 
 class InserimentoSegnalazioneGUI extends StatefulWidget {
   const InserimentoSegnalazioneGUI({super.key});
 
-
   @override
-  State<InserimentoSegnalazioneGUI> createState() => _InserimentoSegnalazioneGUIState();
+  State<InserimentoSegnalazioneGUI> createState() =>
+      _InserimentoSegnalazioneGUIState();
 }
 
-class _InserimentoSegnalazioneGUIState extends State<InserimentoSegnalazioneGUI> {
+class _InserimentoSegnalazioneGUIState
+    extends State<InserimentoSegnalazioneGUI> {
   final _formKey = GlobalKey<FormState>();
- /* String? _categoria;
-  String? _indirizzo;
+  final TextEditingController _indirizzoController = TextEditingController();
+  final TextEditingController _cittaController = TextEditingController();
+
+  /*String? _categoria;
   String? _descrizione;
   String? _titolo;
-*/
-  //XFile? _image;
+  String? _citta;
+  String? _indirizzo;*/
+  List<String>? _indirizzoLista;
+
   @override
   void initState() {
     super.initState();
-    getLocation();
+    _indirizzoController.text = 'Caricamento Posizione...';
+    _cittaController.text = 'Caricamento Città...';
+    _fetchLocation();
+  }
+
+  Future<void> _fetchLocation() async {
+    _indirizzoLista = await getLocation();
+    setState(() {
+      _indirizzoController.text =
+          '${_indirizzoLista!.elementAt(1)} ${_indirizzoLista!.elementAt(2)}';
+      _cittaController.text = _indirizzoLista!.elementAt(0);
+    });
+  }
+
+  @override
+  void dispose() {
+    _indirizzoController.dispose();
+    super.dispose();
   }
 
   @override
@@ -38,75 +60,19 @@ class _InserimentoSegnalazioneGUIState extends State<InserimentoSegnalazioneGUI>
           key: _formKey,
           child: ListView(
             children: [
-              const Text('Titolo', style: TextStyle(fontWeight: FontWeight.bold)),
-              const SizedBox(height: 8),
-              TextFormField(
-                decoration: TextFieldInputDecoration(
-                    context, labelText: 'Titolo'),
-                validator: FormBuilderValidators.required(),
-                onSaved: (value) {
-                  //_titolo = value;
-                },
-              ),
+              _buildTitleField(),
               const SizedBox(height: 16),
-              const Text('Categoria', style: TextStyle(fontWeight: FontWeight.bold)),
-              const SizedBox(height: 8),
-              DropdownButtonFormField<String>(
-                decoration: TextFieldInputDecoration(
-                    context, labelText: 'Categoria'),
-                items: <String>['Categoria 1', 'Categoria 2', 'Categoria 3']
-                    .map((value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-                onChanged: (newValue) {
-                  setState(() {
-                    //_categoria = newValue;
-                  });
-                },
-                validator: FormBuilderValidators.required(),
-              ),
-              const SizedBox(height: 16),
-              const Text('Indirizzo', style: TextStyle(fontWeight: FontWeight.bold)),
-              const SizedBox(height: 8),
-              TextFormField(
-                decoration: TextFieldInputDecoration(
-                    context, labelText: 'Indirizzo'),
-                validator: FormBuilderValidators.required(),
-                onSaved: (value) {
-                 // _indirizzo = value;
-                },
-              ),
-              const SizedBox(height: 16),
-              const Text(
-                  'Descrizione', style: TextStyle(fontWeight: FontWeight.bold)),
-              const SizedBox(height: 8),
-              TextFormField(
-                decoration: TextFieldInputDecoration(
-                    context, labelText: 'Descrizione'),
-                maxLines: 3,
-                validator: FormBuilderValidators.required(),
-                onSaved: (value) {
-                  //_descrizione = value;
-                },
-              ),
+              _buildCategoryField(),
               const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text('Seleziona Foto'),
-              ),
+              _buildCityField(),
               const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _formKey.currentState!.save();
-                    // Handle form submission
-                  }
-                },
-                child: const Text('Invia Segnalazione'),
-              ),
+              _buildAddressField(),
+              const SizedBox(height: 16),
+              _buildDescriptionField(),
+              const SizedBox(height: 20),
+              _buildSelectPhotoButton(),
+              const SizedBox(height: 20),
+              _buildSubmitButton(),
             ],
           ),
         ),
@@ -114,50 +80,159 @@ class _InserimentoSegnalazioneGUIState extends State<InserimentoSegnalazioneGUI>
     );
   }
 
-  Future<void> getLocation() async {
+  Widget _buildTitleField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('Titolo', style: TextStyle(fontWeight: FontWeight.bold)),
+        const SizedBox(height: 8),
+        TextFormField(
+          decoration: TextFieldInputDecoration(context, labelText: 'Titolo'),
+          validator: FormBuilderValidators.required(),
+          onSaved: (value) => {}//_titolo = value,
+        ),
+      ],
+    );
+  }
 
-    //FUNZIONE CHE STAMPA LA LOCATION
+  Widget _buildCategoryField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('Categoria', style: TextStyle(fontWeight: FontWeight.bold)),
+        const SizedBox(height: 8),
+        DropdownButtonFormField<String>(
+          decoration: TextFieldInputDecoration(context, labelText: 'Categoria'),
+          items: <String>['Categoria 1', 'Categoria 2', 'Categoria 3']
+              .map((value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+          onChanged: (newValue) => setState(() => {}), //_categoria = newValue),
+          validator: FormBuilderValidators.required(),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildCityField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('Città', style: TextStyle(fontWeight: FontWeight.bold)),
+        const SizedBox(height: 8),
+        TextFormField(
+          controller: _cittaController,
+          decoration: TextFieldInputDecoration(context, labelText: 'Città'),
+          validator: FormBuilderValidators.required(),
+          onSaved: (value) => {}, //_citta = value,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildAddressField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('Indirizzo', style: TextStyle(fontWeight: FontWeight.bold)),
+        const SizedBox(height: 8),
+        TextFormField(
+          controller: _indirizzoController,
+          decoration: TextFieldInputDecoration(context, labelText: 'Indirizzo'),
+          validator: FormBuilderValidators.required(),
+          onSaved: (value) => {}//_indirizzo = value,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDescriptionField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('Descrizione',
+            style: TextStyle(fontWeight: FontWeight.bold)),
+        const SizedBox(height: 8),
+        TextFormField(
+          decoration:
+              TextFieldInputDecoration(context, labelText: 'Descrizione'),
+          maxLines: 3,
+          validator: FormBuilderValidators.required(),
+          //onSaved: (value) => _descrizione = value,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSelectPhotoButton() {
+    return ElevatedButton(
+      onPressed: () {},
+      child: const Text('Seleziona Foto'),
+    );
+  }
+
+  Widget _buildSubmitButton() {
+    return ElevatedButton(
+      onPressed: () {
+        if (_formKey.currentState!.validate()) {
+          _formKey.currentState!.save();
+          // Handle form submission
+        }
+      },
+      child: const Text('Invia Segnalazione'),
+    );
+  }
+
+  Future<List<String>> getLocation() async {
+    final stopwatch = Stopwatch()..start();
     loc.Location location = loc.Location();
 
-    bool _serviceEnabled;
-    loc.PermissionStatus _permissionGranted;
-
-    // Controlla se il servizio di localizzazione è abilitato
-    _serviceEnabled = await location.serviceEnabled();
-    if (!_serviceEnabled) {
-      _serviceEnabled = await location.requestService();
-      if (!_serviceEnabled) {
-        print('Servizio di localizzazione non abilitato.');
-        return;
+    bool serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        stopwatch.stop();
+        print('Tempo impiegato: ${stopwatch.elapsedMilliseconds} ms');
+        return [
+          'Servizio non abilitato.',
+          'Servizio non abilitato.',
+          'Servizio non abilitato.'
+        ];
       }
     }
 
-    // Controlla i permessi
-    _permissionGranted = await location.hasPermission();
-    if (_permissionGranted == loc.PermissionStatus.denied) {
-      _permissionGranted = await location.requestPermission();
-      if (_permissionGranted == loc.PermissionStatus.denied) {
-        print('Permessi negati.');
-        return;
+    loc.PermissionStatus permissionGranted = await location.hasPermission();
+    if (permissionGranted == loc.PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted == loc.PermissionStatus.denied) {
+        stopwatch.stop();
+        print('Tempo impiegato: ${stopwatch.elapsedMilliseconds} ms');
+        return ['Permessi negati.', 'Permessi negati.', 'Permessi negati.'];
       }
     }
 
-    if (_permissionGranted == loc.PermissionStatus.deniedForever) {
-      print(
-          'Permessi negati permanentemente. Apri le impostazioni per abilitarli.');
-      return;
+    if (permissionGranted == loc.PermissionStatus.deniedForever) {
+      stopwatch.stop();
+      print('Tempo impiegato: ${stopwatch.elapsedMilliseconds} ms');
+      return [
+        'Permessi negati permanentemente.',
+        'Permessi negati permanentemente.',
+        'Permessi negati permanentemente.'
+      ];
     }
 
-    // Ottieni la posizione prima prendo le coordinate (lat e long) e poi stampo solamente la "street" (via
-    // TO-DO : se c'è qualcos altro di utile da stampare
-    //         Dobbiamo gestire i casi in cui non ci sono i permessi per la posizione
-    //      magari possiamo far inserire l indirizzo manualmente ???
-    loc.LocationData _locationData = await location.getLocation();
-    print('Latitudine: ${_locationData.latitude}, Longitudine: ${_locationData
-        .longitude}');
+    loc.LocationData locationData = await location.getLocation();
     List<Placemark> placemarks = await placemarkFromCoordinates(
-        _locationData.latitude!, _locationData.longitude!);
-
-    print(placemarks[0].street);
+        locationData.latitude!, locationData.longitude!);
+    stopwatch.stop();
+    print('Tempo impiegato: ${stopwatch.elapsedMilliseconds} ms');
+    return [
+      placemarks[0].locality ?? "Località non disponibile",
+      placemarks[0].street ?? "Strada non disponibile",
+      placemarks[0].name ?? "Nome non disponibile"
+    ];
   }
 }
