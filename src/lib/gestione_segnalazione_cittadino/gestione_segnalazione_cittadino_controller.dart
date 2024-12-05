842b9ff0b2df92fd42df64f0af8d6de363ffe2fc
GiuseppeGambardella <gagiuseppe29@gmail.com>
2024-12-05 21:38:00 +0100

    Inseriemento Segnalazione Cittadino Controller: tolto import cupertino
    Co-authored-by: FaiellaFrancesco <faiellafrancesco2003@gmail.com>

--- src/lib/gestione_segnalazione_cittadino/gestione_segnalazione_cittadino_controller.dart
+++ src/lib/gestione_segnalazione_cittadino/gestione_segnalazione_cittadino_controller.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../home_page.dart';
 import 'gestione_location_failed.dart';
