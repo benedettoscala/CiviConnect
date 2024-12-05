ba441ee523d5b74a7a36e2f9638a7dd3072c3457
GiuseppeGambardella <gagiuseppe29@gmail.com>
2024-12-05 12:30:53 +0100

    Inserimento Segnalazione Controller: create la funzione che crea il report con le informazioni della segnalazione e chiama il DAO per l'inserimento
    Co-authored-by: FaiellaFrancesco <faiellafrancesco2003@gmail.com>

--- src/lib/gestione_segnalazione_cittadino/gestione_segnalazione_cittadino_controller.dart
+++ src/lib/gestione_segnalazione_cittadino/gestione_segnalazione_cittadino_controller.dart
@@ -1,3 +1,50 @@
-class CitizenReportManagementController{
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
+import '../model/report_model.dart';
+import '../utils/report_status_priority.dart';
+import 'gestione_segnalazione_cittadino_DAO.dart';
+
+class CitizenReportManagementController {
+  final Widget redirectPage;
+
+  CitizenReportManagementController({required this.redirectPage});
+  final CitizenReportManagementDAO _reportDAO = CitizenReportManagementDAO();
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<void> addReport(BuildContext context,
+      {required String citta,
+      required String titolo,
+      required String descrizione,
+      required Category categoria,
+      required GeoPoint location,
+      Map<String, String>? indirizzo}) async {
+    print('$indirizzo nel controller');
+    final report = Report(
+      uid: _firebaseAuth.currentUser!.uid,
+      city: citta,
+      title: titolo,
+      description: descrizione,
+      category: categoria,
+      reportDate: Timestamp.now(),
+      address: indirizzo,
+      location: location,
+      status: StatusReport.inProgress,
+      priority: PriorityReport.low,
+      authorFirstName: '',
+      authorLastName: '',
+      photo: '',
+      endDate: null,
+    );
+    final bool result = await _reportDAO.addReport(report);
+    if (result) {
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => redirectPage),
+        (route) => false,
+      );
+    }
+  }
 }
