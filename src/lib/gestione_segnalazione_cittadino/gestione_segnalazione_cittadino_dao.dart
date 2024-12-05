36f693d1fc9637010eb66c3f901d504feebd888b
Type-Here <94553428+Type-Here@users.noreply.github.com>
2024-12-05 02:10:13 +0100

    gestione_segnalazione_cittadino_dao.dart: fixed get report path to documents and data retrieval but offset not implemented yet

--- src/lib/gestione_segnalazione_cittadino/gestione_segnalazione_cittadino_dao.dart
+++ src/lib/gestione_segnalazione_cittadino/gestione_segnalazione_cittadino_dao.dart
@@ -21,18 +21,18 @@ class CitizenReportManagementDAO {
   /// - [city]: The name of the city for which to retrieve the reports.
   ///
   /// Returns:
-  /// - A `Future<List<Map<String, dynamic>>>` containing the list of reports for the specified city, or `null` if the user is not valid.
+  /// - A `Future<List<Map<String, dynamic>>?>` containing the list of reports for the specified city, or `null` if the user is not valid.
   ///
   /// Throws:
   /// - [Exception]: If the user is not logged in.
   ///
   /// Preconditions:
   /// - The user must be logged in and either a `Municipality` with the specified city name or a `Citizen`.
-  Future<List<Map<String, dynamic>>>? getReportList(String city){
-      if(!_checkForUserValidity(city)) {
+  Future<List<Map<String, dynamic>>?> getReportList(String city) async {
+      if(! await _checkForUserValidity(city)) {
         return null;
       }
-      return _getTenReportsByOffset(city: city);
+      return await _getTenReportsByOffset(city: city);
   }
 
 
@@ -46,46 +46,22 @@ class CitizenReportManagementDAO {
   ///
   /// Returns:
   /// - A `Future<List<Map<String, dynamic>>>` containing the next ten reports for the specified city.
-  ///
-  /// Throws:
-  /// - [Exception]: If there is an error retrieving the data.
-  Future<List<Map<String, dynamic>>> _getTenReportsByOffset({required String city}) async {
-    //final reports = await _firestore.collection(getPath(city, null)).orderBy('creationDate', descending: true).startAt([offset]).limit(10).get();
-    final first = _firestore.collection(_getPath(city, null)).orderBy('creationDate', descending: true).limit(10);
-    return first.get().then(
-          (documentSnapshots) {
-        // Get the last visible document
-        final lastVisible = documentSnapshots.docs[documentSnapshots.size - 1];
-
-        // Construct a new query starting at this document,
-        // get the next 25 cities.
-        final next = _firestore
-            .collection(_getPath(city, null))
-            .orderBy('creationDate', descending: true)
-            .startAfterDocument(lastVisible)
-            .limit(10);
-      },
-      onError: (e) => throw Exception('Errore nella ricerca dei dati'),
-    ) as List<Map<String, dynamic>>;
+  ////
+/// Throws:
+/// - [Exception]: If there is an error retrieving the data.
+Future<List<Map<String, dynamic>>?> _getTenReportsByOffset({required String city}) async {
+  final first = _firestore.collection('reports').doc(city.toLowerCase()).collection('${city.toLowerCase()}_reports')
+    .orderBy('title', descending: true).limit(10);
+  try {
+    final querySnapshot = await first.get();
+    final data = querySnapshot.docs.map((doc) => doc.data()).toList();
+    return data;
+  } catch (e) {
+    throw Exception('Error retrieving data: $e');
   }
-
-
+}
   /* -------------------------------- UTILITY PRIVATE METHODS ----------------------------- */
 
-  /// Returns the path to the report(s) in the database for the given city and report ID.
-  ///
-  /// Parameters:
-  /// - [city]: The name of the city.
-  /// - [reportId]: The unique identifier of the report. If `null`, returns the path to all reports in the specified city.
-  ///
-  /// Returns:
-  /// - A `String` representing the path to the report(s) in the database.
-  String _getPath(String city, String? reportId) {
-    if (reportId == null) {
-      return 'reports/$city/${city}_reports';
-    }
-    return 'reports/$city/${city}_reports/$reportId';
-  }
 
   /// Checks if the current user is valid for the given city.
   ///
@@ -97,8 +73,8 @@ class CitizenReportManagementDAO {
   ///
   /// Throws:
   /// - [Exception]: If the user is not logged in.
-  bool _checkForUserValidity(String? city) {
-    GenericUser? user = _userManagementDAO.getUser;
+  Future<bool> _checkForUserValidity(String? city) async {
+    GenericUser? user = await _userManagementDAO.determineUserType();
     if (user == null) {
       throw Exception('Utente non loggato!');
     }
