f0a0a036e6dcf76c11c930668ff9a2b91f2665b6
GiuseppeGambardella <gagiuseppe29@gmail.com>
2024-12-05 14:05:35 +0100

    Gestione Segnalazione Cittadino Controller: aggiunti controlli sui permessi e geolocalizzazione
    Co-authored-by: FaiellaFrancesco <faiellafrancesco2003@gmail.com>

--- src/lib/gestione_segnalazione_cittadino/gestione_segnalazione_cittadino_controller.dart
+++ src/lib/gestione_segnalazione_cittadino/gestione_segnalazione_cittadino_controller.dart
@@ -1,15 +1,20 @@
+import 'dart:ffi';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'gestione_location_failed.dart';
 import '../model/report_model.dart';
 import '../utils/report_status_priority.dart';
 import 'gestione_segnalazione_cittadino_DAO.dart';
+import 'package:location/location.dart' as loc;
+import 'package:geocoding/geocoding.dart';
 
 class CitizenReportManagementController {
   final Widget redirectPage;
 
+  late GeoPoint _location;
   CitizenReportManagementController({required this.redirectPage});
   final CitizenReportManagementDAO _reportDAO = CitizenReportManagementDAO();
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
@@ -21,7 +26,6 @@ class CitizenReportManagementController {
       required Category categoria,
       required GeoPoint location,
       Map<String, String>? indirizzo}) async {
-    print('$indirizzo nel controller');
     final report = Report(
       uid: _firebaseAuth.currentUser!.uid,
       city: citta,
@@ -47,4 +51,65 @@ class CitizenReportManagementController {
       );
     }
   }
+}
+
+Future<GeoPoint?> getCoordinates(BuildContext context) async {
+  final stopwatch = Stopwatch()
+    ..start();
+  loc.Location location = loc.Location();
+
+  bool serviceEnabled = await location.serviceEnabled();
+  if (!serviceEnabled) {
+    serviceEnabled = await location.requestService();
+    if (!serviceEnabled) {
+      stopwatch.stop();
+      print('Tempo impiegato: ${stopwatch.elapsedMilliseconds} ms');
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (context) =>
+                LocationPermissionPage(
+                  onRetry: () => getCoordinates(context),
+                )),
+      );
+      return null;
+    }
+  }
+
+  loc.PermissionStatus permissionGranted = await location.hasPermission();
+  if (permissionGranted == loc.PermissionStatus.denied) {
+    permissionGranted = await location.requestPermission();
+    if (permissionGranted == loc.PermissionStatus.denied) {
+      stopwatch.stop();
+      print('Tempo impiegato: ${stopwatch.elapsedMilliseconds} ms');
+      return null;
+    }
+  }
+
+  if (permissionGranted == loc.PermissionStatus.deniedForever) {
+    stopwatch.stop();
+    print('Tempo impiegato: ${stopwatch.elapsedMilliseconds} ms');
+
+    return null;
+  }
+
+  loc.LocationData locationData = await location.getLocation();
+  return GeoPoint(locationData.latitude!, locationData.longitude!);
+}
+
+Future<List<String>> getLocation(GeoPoint location) async {
+  final stopwatch = Stopwatch()
+    ..start();
+  final locationData = location;
+
+//setting posizione
+  List<Placemark> placemarks = await placemarkFromCoordinates(
+      locationData.latitude!, locationData.longitude!);
+  stopwatch.stop();
+  print('Tempo impiegato: ${stopwatch.elapsedMilliseconds} ms');
+  return [
+    placemarks[0].locality ?? "Località non disponibile",
+    placemarks[0].street ?? "Strada non disponibile",
+    placemarks[0].name ?? "Nome non disponibile"
+  ];
 }
