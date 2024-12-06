b0563d4850fc896f31f10dc6d6aa7c7abc67a777
Type-Here <94553428+Type-Here@users.noreply.github.com>
2024-12-06 01:28:21 +0100

    gestione_segnalazione_cittadino_dao: now documentSnapshot lastDocument is an attribute of the dao, added _isEnded boot to correctly stop at the end of query

--- src/lib/gestione_segnalazione_cittadino/gestione_segnalazione_cittadino_dao.dart
+++ src/lib/gestione_segnalazione_cittadino/gestione_segnalazione_cittadino_dao.dart
@@ -10,6 +10,11 @@ class CitizenReportManagementDAO {
   final UserManagementDAO _userManagementDAO;
   /// The Firestore instance.
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  /// The last document retrieved in the previous query.
+  DocumentSnapshot? _lastDocument;
+
+  /// Specifies it the query offset is at the bottom of the list.
+  bool _isEnded = false;
 
 
   /// Constructs a new `CitizenReportManagementDAO` instance.
@@ -32,11 +37,17 @@ class CitizenReportManagementDAO {
   ///
   /// Preconditions:
   /// - The user must be logged in and either a `Municipality` with the specified city name or a `Citizen`.
-  Future<List<Map<String, dynamic>>?> getReportList({required String city, DocumentSnapshot? lastDocument, bool? reset}) async {
-      if(! await _checkForUserValidity(city)) {
+  Future<List<Map<String, dynamic>>?> getReportList({required String city, bool? reset}) async {
+      if(reset == true){
+        _isEnded = false;
+        _lastDocument = null;
+      }
+
+      if(! await _checkForUserValidity(city) || _isEnded){
         return null;
       }
-      return await _getTenReportsByOffset(city: city, lastDocument: lastDocument, reset: reset);
+
+      return await _getTenReportsByOffset(city: city);
   }
 
 
@@ -54,25 +65,33 @@ class CitizenReportManagementDAO {
   ///
   /// Throws:
   /// - [Exception]: If there is an error retrieving the data.
-Future<List<Map<String, dynamic>>?> _getTenReportsByOffset({required String city, DocumentSnapshot? lastDocument, bool? reset}) async {
+Future<List<Map<String, dynamic>>?> _getTenReportsByOffset({required String city}) async {
   Query<Map<String, dynamic>> query = _firestore.collection('reports').doc(city.toLowerCase()).collection('${city.toLowerCase()}_reports')
     .orderBy('title', descending: true).limit(10);
 
-  if (lastDocument != null) {
-    query = query.startAfterDocument(lastDocument);
-  }
-
-  if(reset == true){
-    lastDocument = null;
+  // If the last document is not null, the query starts after the last document of the previous query.
+  if (_lastDocument != null) {
+    query = query.startAfterDocument(_lastDocument!);
   }
 
   try {
     final querySnapshot = await query.get();
-    if (querySnapshot.docs.isEmpty) {
+    // If the query is empty or the last document is the same as the previous one, the query is ended.
+    if (_isEnded || querySnapshot.docs.isEmpty || _lastDocument == querySnapshot.docs.last){
+      _isEnded = true;
       return null;
     }
+
+    // If the query is less than 10, the query is ended but the last documents are updated.
+    if(querySnapshot.docs.length < 10){
+      _isEnded = true;
+    }
+
+    //Retrieve the data from the query snapshot.
     var data = querySnapshot.docs.map((doc) => doc.data()).toList();
-    data.add({'lastDocument' : querySnapshot.docs.last});
+    // Update the last document.
+    _lastDocument  = querySnapshot.docs.last;
+
     return data;
 
   } catch (e) {
@@ -119,7 +138,7 @@ class MockCitizenReportManagementDAO extends Mock implements CitizenReportManage
   late final List<Map<String, dynamic>> reports;
 
   /// The index of the last document retrieved. MOCK
-  int _lastDocument = 0;
+  int _lastDocumentIndex = 0;
 
   /// Constructs a new `MockCitizenReportManagementDAO` instance.
   /// Parameters:
@@ -153,23 +172,20 @@ class MockCitizenReportManagementDAO extends Mock implements CitizenReportManage
   }
 
   @override
-  Future<List<Map<String, dynamic>>?> getReportList({required String city, DocumentSnapshot? lastDocument, bool? reset}) async {
+  Future<List<Map<String, dynamic>>?> getReportList({required String city, bool? reset}) async {
     if(reset == true){
-      _lastDocument = 0;
+      _lastDocumentIndex = 0;
     }
 
-    if(_lastDocument == 0){
-      _lastDocument = 10;
-     return Future.value(reports.sublist(0, 10));
-
-    } else {
-      if(_lastDocument >= 30){
-        return Future.value(null);
-      }
-      int index = _lastDocument;
-      _lastDocument = index + 10;
-      return Future.value(reports.sublist(index, index + 10));
+    if(_lastDocumentIndex >= 30){
+      return Future.value(null);
     }
+
+    int index = _lastDocumentIndex;
+    _lastDocumentIndex = index + 10;
+    return Future.value(reports.sublist(index, index + 10));
   }
+
+
 
 }
