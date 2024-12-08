cb777ae4486dd46e2a4cc4b7758f8b0c8a9c7ec1
Type-Here <94553428+Type-Here@users.noreply.github.com>
2024-12-08 01:43:56 +0100

    gestione_analisi_dati_dao: added retriveDataForAnalysis method return map fro piechart

--- src/lib/analisi_dati/gestione_analisi_dati_dao.dart
+++ src/lib/analisi_dati/gestione_analisi_dati_dao.dart
@@ -3,6 +3,22 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_map_heatmap/flutter_map_heatmap.dart';
 import 'package:latlong2/latlong.dart';
 
+/// The data partition to use for the analysis.
+/// The data partition is used to divide the data into different categories.
+/// The categories are:
+/// - Priority
+/// - Status
+/// - Category
+enum DataPartition {
+  /// Analyze data by priority of the reports.
+  priority,
+  /// Analyze data by status of the reports.
+  status,
+  /// Analyze data by category of the reports.
+  category
+}
+
+
 /// Data Access Object (DAO) for managing data analysis.
 /// The DAO is used to retrieve data from the database
 /// to be used in the data analysis for the municipality.
@@ -14,6 +30,10 @@ class DataAnalysisManagementDAO {
   /// The HeatMap data retrieved from the database.
   /// Whe DAO is used to retrieve the data, the data is stored in this variable to cache it.
   List<WeightedLatLng>? _data;
+
+  /// The document snapshot retrieved from the database.
+  /// It contains all reports for a specific city.
+  List<QueryDocumentSnapshot<Map<String, dynamic>>>? _docs;
 
   /// The default point to use if no data is available.
   /// This point is the coordinates of Fisciano City.
@@ -29,9 +49,9 @@ class DataAnalysisManagementDAO {
     if(_data != null){
       return _data;
     }
+
     // Retrieve the data from the database.
-    final querySnapshot = await _firestore.collection('reports').doc(city).collection('${city}_reports').get();
-    final docs = querySnapshot.docs;
+    final docs = await _retrieveReports(city: city);
 
     // If no data is available, return null.
     if(docs.isEmpty){
@@ -63,6 +83,63 @@ class DataAnalysisManagementDAO {
       final point = docs.first.data()['location'] ?? _defaultPoint;
       return LatLng(point.latitude, point.longitude);
     });
+  }
+
+  /// Retrieves the data for the analysis.
+  /// The data is retrieved from the database and analyzed based on the data partition.
+  /// - [city]: The name of the city for which to retrieve the data.
+  /// - [dataPartition]: The data partition to use for the analysis.
+  /// Returns:
+  /// - A `Future<Map<String, double>?>` containing the partitioned data.
+  Future<Map<String, double>?> retrieveDataForAnalysis({
+    String? city,
+    DataPartition? dataPartition,
+  }) async {
+
+    if(_docs == null){
+      await _retrieveReports(city: city);
+    }
+
+    if(_docs!.isEmpty){
+      return null;
+    }
+
+    switch(dataPartition){
+      case DataPartition.status:
+        return _analyzeDataBy('status');
+      case DataPartition.category:
+        return _analyzeDataBy('category');
+      case DataPartition.priority:
+        return _analyzeDataBy('priority');
+      default:
+        return null;
+    }
+
+  }
+
+
+  /* ----------------- Private methods ----------------- */
+
+  /// Retrieves all reports for a specific city.
+  /// - [city]: The name of the city for which to retrieve the reports.
+  /// Returns:
+  /// - A `Future<QuerySnapshot>` containing all reports for the city.
+  Future<List<QueryDocumentSnapshot<Map<String, dynamic>>>> _retrieveReports({String? city}) async {
+    // Retrieve the data from the database.
+    final querySnapshot = await _firestore.collection('reports').doc(city).collection('${city}_reports').get();
+    final docs = querySnapshot.docs;
+    _docs = docs;
+    return docs;
+  }
+
+
+  Map<String, double> _analyzeDataBy(String characteristic) {
+    final mapData = <String, double>{};
+    for (var doc in _docs!) {
+      final key = doc.data()[characteristic] ?? 'Non impostato';
+      mapData[key] = (mapData[key] ?? 0) + 1;
+    }
+    return mapData;
   }
 
 }
