5ab9a0342992e17e421dc596fbdf5759be8eb405
Type-Here <94553428+Type-Here@users.noreply.github.com>
2024-12-08 18:44:42 +0100

    home_page: removed duplicate import

--- src/lib/home_page.dart
+++ src/lib/home_page.dart
@@ -5,7 +5,6 @@ import 'package:civiconnect/user_management/user_management_dao.dart';
 import 'package:civiconnect/user_management/user_profile_gui.dart';
 import 'package:flutter/material.dart';
 
-import 'gestione_admin/admin_gui.dart';
 import 'gestione_segnalazione_cittadino/visualizzazione_segnalazioni_gui.dart';
 import 'model/users_model.dart';
 
