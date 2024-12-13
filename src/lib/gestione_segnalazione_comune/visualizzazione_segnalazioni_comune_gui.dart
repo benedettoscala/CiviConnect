e15888cda135d1601b4b795f487795144cc733cb
Type-Here <94553428+Type-Here@users.noreply.github.com>
2024-12-13 02:48:20 +0100

    visualizzazione_segnalazioni_comune_gui: added filterData, modal call on filter button, reset filter and dispose methods
    Co-authored-by: Borax-Gargamella <157167477+borax-gargamella@users.noreply.github.com>

--- src/lib/gestione_segnalazione_comune/visualizzazione_segnalazioni_comune_gui.dart
+++ src/lib/gestione_segnalazione_comune/visualizzazione_segnalazioni_comune_gui.dart
@@ -1,6 +1,8 @@
 import 'package:civiconnect/gestione_segnalazione_comune/dettagli_segnalazione_comune_gui.dart';
+import 'package:civiconnect/model/users_model.dart';
 import 'package:civiconnect/theme.dart';
 import 'package:civiconnect/utils/report_status_priority.dart';
+import 'package:civiconnect/widgets/filter_modal.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:hugeicons/hugeicons.dart';
@@ -27,6 +29,13 @@ class _ReportsListMunicipalityState extends State<ReportsViewMunicipalityGUI> {
   bool _isLoading = true;
   String _errorText = '';
   late ScrollController _scrollController;
+  Municipality? _municipality;
+  int _numberOfFilters = 0;
+  final List<StatusReport> _statusCriteria = [];
+  final List<PriorityReport> _priorityCriteria = [];
+  final List<Category> _categoryCriteria = [];
+  String? _keyWords;
+  DateTimeRange? _dateRange;
 
   /// Initializes the state of the widget.
   ///
@@ -47,6 +56,12 @@ class _ReportsListMunicipalityState extends State<ReportsViewMunicipalityGUI> {
     _reportController = MunicipalityReportManagementController(context: context);
     theme = ThemeManager().customTheme;
     _loadInitialData(); // Load initial data
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose(); // Libera il controller dello scroll
+    super.dispose();
   }
 
   /// Build the widget
@@ -78,6 +93,7 @@ class _ReportsListMunicipalityState extends State<ReportsViewMunicipalityGUI> {
     });
     try {
       _reportController.municipality.then((value) {
+        _municipality = value;
         _reportController.getMunicipalityReports(reset: true).then((value) {
           _userData.clear();
           setState(() {
@@ -196,26 +212,69 @@ class _ReportsListMunicipalityState extends State<ReportsViewMunicipalityGUI> {
         Padding(
           padding: const EdgeInsets.all(10.0),
           child: Card(
-            color: Colors.white70,
+            color: _numberOfFilters > 0
+                ? Theme.of(context).colorScheme.primaryContainer
+                : Colors.white70,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(12),
             ),
             elevation: 2,
             shadowColor: Colors.black.withOpacity(0.5),
             child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+              padding:
+              const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
               child: Row(
                 mainAxisSize: MainAxisSize.min,
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  IconButton(
-                    icon: Icon(
-                      Icons.filter_list,
-                      color: theme.colorScheme.onPrimaryContainer,
-                    ),
-                    onPressed: () {
-                      // TODO: Implementa il metodo di selezione del filtro
-                    },
+
+                  /// Filter Button
+                  Stack(
+                    clipBehavior: Clip.none,
+                    children: [
+                      IconButton(
+                        icon: Icon(
+                          Icons.filter_list,
+                          color: theme.colorScheme.onPrimaryContainer,
+                        ),
+                        onPressed: () {
+                          /// Show the filter modal
+                          showModalBottomSheet(
+                              context: context,
+                              isScrollControlled: true,
+                              builder: (context) => _municipality != null
+                                  ? FilterModal(
+                                dateRange: _dateRange,
+                                defaultCity: _municipality!.municipalityName ?? '',
+                                categoryCriteria: _categoryCriteria,
+                                statusCriteria: _statusCriteria,
+                                priorityCriteria: _priorityCriteria,
+                                startCity: _municipality!.municipalityName ?? '',
+                                onSubmit: _filterData,
+                                onReset: _resetFilters,
+                              )
+                                  : const SizedBox());
+                        },
+                      ),
+
+                      /// Show the number of filters applied if there are any
+                      if (_numberOfFilters > 0)
+                        Positioned(
+                          right: -5,
+                          top: -5,
+                          child: CircleAvatar(
+                            radius: 8,
+                            backgroundColor: Colors.red,
+                            child: Text(
+                              _numberOfFilters.toString(),
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                              ),
+                            ),
+                          ),
+                        ),
+                    ],
                   ),
                 ],
               ),
@@ -274,7 +333,9 @@ class _ReportsListMunicipalityState extends State<ReportsViewMunicipalityGUI> {
                   context,
                   MaterialPageRoute(
                       builder: (context) =>
-                          DettagliSegnalazioneComune(report: store)));
+                          DettagliSegnalazioneComune(report: store)
+                  ),
+              );
             },
           );
         },
@@ -287,8 +348,18 @@ class _ReportsListMunicipalityState extends State<ReportsViewMunicipalityGUI> {
   /// This method reloads the initial data and waits for a short delay before completing.
   Future<void> _pullRefresh() async {
     _loadInitialData();
+    _categoryCriteria.clear();
+    _statusCriteria.clear();
+    _priorityCriteria.clear();
+    setState(() {
+      _dateRange = null;
+      _numberOfFilters = 0;
+      _keyWords = '';
+      _errorText = '';
+    });
     await Future.delayed(const Duration(seconds: 1));
   }
+
 
   /// Builds the search bar
   /// /// Builds the search bar widget.
@@ -318,12 +389,15 @@ class _ReportsListMunicipalityState extends State<ReportsViewMunicipalityGUI> {
                   color: theme.colorScheme.onPrimaryContainer,
                 ),
                 onPressed: () {
-                  // TODO - Implement search functionality
+                  if (_keyWords != null && _keyWords!.isNotEmpty ||
+                      _numberOfFilters > 0) {
+                    _filterData(city: _municipality?.municipalityName, keyWords: _keyWords, popNav: false, isCityEnabled: false);
+                  }
                 },
               ),
-              const Flexible(
+              Flexible(
                 child: TextField(
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                       fillColor: Colors.white,
                       filled: true,
                       border: OutlineInputBorder(
@@ -331,6 +405,14 @@ class _ReportsListMunicipalityState extends State<ReportsViewMunicipalityGUI> {
                         borderSide: BorderSide.none,
                       ),
                       hintText: 'Cerca segnalazione...'),
+                  onChanged: (value) {
+                    _keyWords = value;
+                  },
+                  onSubmitted: (value) {
+                    if (value.isNotEmpty || _numberOfFilters > 0) {
+                      _filterData(city: _municipality?.municipalityName, keyWords: _keyWords, popNav: false, isCityEnabled: false);
+                    }
+                  },
                 ),
               ),
             ],
@@ -339,4 +421,82 @@ class _ReportsListMunicipalityState extends State<ReportsViewMunicipalityGUI> {
       ),
     );
   }
+
+
+  /// Filters the data based on the selected filters.
+  /// Parameters:
+  /// - status: The list of selected status filters.
+  /// - priority: The list of selected priority filters.
+  /// - category: The list of selected category filters.
+  /// - city: The city to filter the reports by.
+  /// This method fetches the reports based on the selected filters and updates the state with the new data.
+  /// If an error occurs during the filtering process, the error message is displayed.
+  Future<void> _filterData(
+      { String? city,
+        List<StatusReport>? status,
+        List<PriorityReport>? priority,
+        List<Category>? category,
+        String? keyWords,
+        DateTimeRange? dateRange,
+        bool? isCityEnabled,
+        bool? popNav = false}) async {
+
+    _numberOfFilters = 0;
+    _numberOfFilters += status?.length ?? 0;
+    _numberOfFilters += priority?.length ?? 0;
+    _numberOfFilters += category?.length ?? 0;
+    _numberOfFilters += dateRange != null ? 1 : 0;
+
+    setState(() {
+      _isLoading = true;
+      _hasMoreData = true;
+      _errorText = '';
+      _dateRange = dateRange;
+    });
+
+    try {
+        _reportController
+            .filterReportsBy(
+            status: status,
+            priority: priority,
+            category: category,
+            dateRange: dateRange,
+            keyword: keyWords)
+            .then((value) async {
+
+          _userData.clear();
+
+          if (mounted) {
+            setState(() {
+              if (value != null && value.isNotEmpty) {
+                _userData.addAll(value);
+              } else {
+                _errorText = 'Nessuna segnalazione trovata';
+              }
+            });
+          }
+        });
+
+    } catch (e) {
+      _errorText = 'Errore durante il caricamento filtrato: $e';
+    } finally {
+      if (popNav!) {
+        Navigator.pop(context);
+      }
+      setState(() {
+        _isLoading = false;
+        _hasMoreData = false;
+      });
+    }
+  }
+
+  /// Resets the filters and reloads the initial data.
+  /// This method resets the filters and reloads the initial data.
+  void _resetFilters() async {
+    Navigator.pop(context);
+    await _pullRefresh();
+  }
+
+
+
 }
