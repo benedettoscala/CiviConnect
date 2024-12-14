8b08c882fe9a53fa68c5b9e81ee4b0d45ce752ab
Type-Here <94553428+Type-Here@users.noreply.github.com>
2024-12-14 06:01:25 +0100

    user_management_controller: constructor to get dao for injection and set attribute for better management

--- src/lib/user_management/user_management_controller.dart
+++ src/lib/user_management/user_management_controller.dart
@@ -14,11 +14,15 @@ class UserManagementController {
   /// The page to navigate to after a successful login.
   final Widget? redirectPage;
 
+  /// The DAO for user management operations.
+  final UserManagementDAO userManagementDAO;
+
   /// Constructs a `UserManagementController` instance.
   ///
   /// Parameters:
   /// - [redirectPage]: The target page to navigate to after a successful login.
-  UserManagementController({this.redirectPage});
+  UserManagementController({this.redirectPage, userManagementDAO})
+      : userManagementDAO = userManagementDAO ?? userManagementDAO();
 
   /// Handles user login.
   ///
@@ -49,7 +53,7 @@ class UserManagementController {
   /// Returns:
   /// - A `Future<bool>` indicating whether the authentication was successful.
   Future<bool> _validateAuth(BuildContext context, email, password) async {
-    final bool result = await UserManagementDAO()
+    final bool result = await userManagementDAO
         .signInWithEmailAndPassword(email: email, password: password);
 
     if (result) {
@@ -68,7 +72,7 @@ class UserManagementController {
   /// Returns:
   /// - A `Future<Map<String, dynamic>>` containing the user's data.
   Future<Map<String, dynamic>> getUserData() async {
-    return await UserManagementDAO().getUserData();
+    return await userManagementDAO.getUserData();
   }
 
   /// Fetches the municipality data.
@@ -76,7 +80,7 @@ class UserManagementController {
   /// Returns:
   /// - A `Future<Map<String, String>>` containing the municipality data.
   Future<Map<String, String>> getMunicipalityData() async {
-    return await UserManagementDAO().getMunicipalityData();
+    return await userManagementDAO.getMunicipalityData();
   }
 
   /// Updates the user's data.
@@ -84,7 +88,7 @@ class UserManagementController {
   /// Parameters:
   /// - [userData]: A map containing the user's updated data.
   Future<void> updateUserData(Map<String, dynamic> userData) async {
-    await UserManagementDAO().updateUserData(userData);
+    await userManagementDAO.updateUserData(userData);
   }
 
   // -------------------- User Data Modification Methods --------------------
@@ -99,7 +103,7 @@ class UserManagementController {
   /// - [currentPassword]: The user's current password for authentication.
   Future<void> changeEmail(BuildContext context,
       {required String newEmail, required String currentPassword}) async {
-    await UserManagementDAO()
+    await userManagementDAO
         .updateEmail(newEmail: newEmail, currentPassword: currentPassword);
   }
 
@@ -113,7 +117,7 @@ class UserManagementController {
   /// - [newPassword]: The new password to set.
   Future<void> changePassword(BuildContext context,
       {required String currentPassword, required String newPassword}) async {
-    await UserManagementDAO().updatePassword(
+    await userManagementDAO.updatePassword(
         currentPassword: currentPassword, newPassword: newPassword);
   }
 
@@ -175,7 +179,7 @@ class UserManagementController {
 
   Future<bool> _validateRegistration(BuildContext context, email, password,
       name, surname, address, city, cap) async {
-    UserManagementDAO userDao = UserManagementDAO();
+    UserManagementDAO userDao = userManagementDAO;
     final bool result = await userDao.createUserWithEmailAndPassword(
         email: email,
         password: password!,
@@ -200,7 +204,7 @@ class UserManagementController {
 
   /// Checks if the current user is an administrator.
   Future<bool> checkIfUserIsAdmin() async {
-    return await UserManagementDAO().determineUserType() is Admin;
+    return await userManagementDAO.determineUserType() is Admin;
   }
 
   /// By Marco: MI MANCA MARTINA :(
@@ -219,7 +223,7 @@ class UserManagementController {
   /// - An exception if an error occurs during the process.
   Future<void> logOut() async {
     try {
-      await UserManagementDAO().logOut();
+      await userManagementDAO.logOut();
     } catch (e) {
       throw Exception('Errore durante il logout');
     }
@@ -245,10 +249,12 @@ class UserManagementController {
   /// }
   /// ```
 Future<GenericUser?> determineUserType() async {
-  return await UserManagementDAO().determineUserType();
+  return await userManagementDAO.determineUserType();
 }
 
+
+  /// Returns the current user.
   User? getcurrentUser() {
-    return UserManagementDAO().currentUser;
+    return userManagementDAO.currentUser;
   }
 }
