407cec74fc6b426a235c23a3d96e9bf1e82acc92
Borax-Gargamella <157167477+Borax-Gargamella@users.noreply.github.com>
2024-12-14 20:20:31 +0100

    run Dart Analyze, Dart Fix and Dart Format

--- src/test/status_change_test.dart
+++ src/test/status_change_test.dart
@@ -12,7 +12,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
-
 /// Test Case for Modifica Stato Segnalazione
 ///
 ///
@@ -25,30 +24,38 @@ import 'package:mockito/mockito.dart';
 
 // Mock del ReportDAO
 class MockReportDAO extends Mock implements MunicipalityReportManagementDAO {}
+
 class MockUserDAO extends Mock implements UserManagementDAO {}
+
 class MockBuildContext extends Mock implements BuildContext {}
+
 class MockUser extends Mock implements User {}
 
-Municipality m= Municipality(user: MockUser(), municipalityName: 'municipalityName');
+Municipality m =
+    Municipality(user: MockUser(), municipalityName: 'municipalityName');
 
-Report r= Report(
+Report r = Report(
     city: 'testCity',
     reportId: 'report123',
     priority: PriorityReport.high,
     status: StatusReport.completed,
     title: 'title',
     description: 'description',
-    reportDate:  Timestamp.now(),
+    reportDate: Timestamp.now(),
     address: {'street': 'N/A', 'number': 'N/A'},
     location: const GeoPoint(0, 0),
-    photo: '', uid: 'uid', authorFirstName: 'authorFirstName', authorLastName: 'authorLastName'
-);
+    photo: '',
+    uid: 'uid',
+    authorFirstName: 'authorFirstName',
+    authorLastName: 'authorLastName');
 
 class FakeReportDAO extends Fake implements MunicipalityReportManagementDAO {
   @override
-  Future<void> editReportStatus({required String? city,
+  Future<void> editReportStatus({
+    required String? city,
     required String? reportId,
-    required StatusReport newStatus,}) async {
+    required StatusReport newStatus,
+  }) async {
     r.status = newStatus;
   }
 }
@@ -58,19 +65,37 @@ void main() {
   // enum not valid is tested by enum itself
 
   /// TC_7.0.2 StatusReport Scartata Expected: changeStatus Fails
-  _testState(description: 'TC_7.0.2 StatusReport Scartata Expected: changeStatus Fails', currentStatus: StatusReport.rejected, newStatus: StatusReport.inProgress, expectedStatus: StatusReport.rejected);
+  _testState(
+      description:
+          'TC_7.0.2 StatusReport Scartata Expected: changeStatus Fails',
+      currentStatus: StatusReport.rejected,
+      newStatus: StatusReport.inProgress,
+      expectedStatus: StatusReport.rejected);
 
   /// TC_7.0.3 StatusReport current After new Expected: changeStatus Fails
-  _testState(description: 'TC_7.0.3 StatusReport current After new Expected: changeStatus Fails', currentStatus: StatusReport.completed, newStatus: StatusReport.inProgress, expectedStatus: StatusReport.completed);
+  _testState(
+      description:
+          'TC_7.0.3 StatusReport current After new Expected: changeStatus Fails',
+      currentStatus: StatusReport.completed,
+      newStatus: StatusReport.inProgress,
+      expectedStatus: StatusReport.completed);
 
   /// TC_7.0.4 StatusReport current Before new and valid Expected: changeStatus is Accepted
-  _testState(description: 'TC_7.0.3 StatusReport current After new Expected: changeStatus Fails', currentStatus: StatusReport.inProgress, newStatus: StatusReport.completed, expectedStatus: StatusReport.completed);
+  _testState(
+      description:
+          'TC_7.0.3 StatusReport current After new Expected: changeStatus Fails',
+      currentStatus: StatusReport.inProgress,
+      newStatus: StatusReport.completed,
+      expectedStatus: StatusReport.completed);
 }
 
-void _testState({required description, required StatusReport currentStatus, required StatusReport newStatus, required StatusReport expectedStatus}) {
-
-  final mockReportDAO = FakeReportDAO();  // Istanza del mock del DAO
-  final mockUserDAO=MockUserDAO();
+void _testState(
+    {required description,
+    required StatusReport currentStatus,
+    required StatusReport newStatus,
+    required StatusReport expectedStatus}) {
+  final mockReportDAO = FakeReportDAO(); // Istanza del mock del DAO
+  final mockUserDAO = MockUserDAO();
   const city = 'testCity';
   const reportId = 'report123';
 
