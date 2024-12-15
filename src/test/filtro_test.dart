39b86e7b839c2944771a9611a793ea59dda2932b
FaiellaFrancesco <faiellafrancesco2003@gmail.com>
2024-12-15 12:45:05 +0100

    Removed redundant comments and prints, fixed warnings, complete documentation, optimized code, and applied refactoring

--- src/test/filtro_test.dart
+++ src/test/filtro_test.dart
@@ -5,21 +5,19 @@ import 'package:form_builder_validators/form_builder_validators.dart';
 //import 'package:form_builder_validators/form_builder_validators.dart';
 
 
-/// | **Test Case ID** | **Test Frame**          | **Esito**                         |
+/// | **Test Case ID** | **Test Frame**          | **Outcome**                       |
 /// |------------------|-------------------------|------------------------------------|
-/// | TC_6.0_1         | VC1                     | Errore: Categoria non valida      | enum is tested by the enum itself
-/// | TC_6.0_2         | VC2 FD1                 | Errore: Formato data non valido   | enum is tested by the enum itself
-/// | TC_6.0_3         | VC2 FD2 VD1             | Errore: Data non valida           | enum is tested by the enum itself
-/// | TC_6.0_4         | VC2 FD2 VD2 VP1         | Errore: Priorità non valida       | enum is tested by the enum itself
-/// | TC_6.0_5         | VC2 FD2 VS2 VP2 VS1     | Errore: Stato non valido          | enum is tested by the enum itself
-/// | TC_6.0_6         | VC2 FD2 VS2 VP2 VS2     | Corretto                          |
-/// | TC_6.0_7         | LR1                     | Errore: Lunghezza ricerca non corretta    |
-/// | TC_6.0_8         | LR2                    | Corretto                           |
-/// | TC_6.0_9         | LA1                     | Errore: Lunghezza Comune non corretta |
-/// | TC_6.0_10         | LA2                     | Corretto                          |
-/// | TC_6.0_11        | VC2 FD2 VS2 VP2 VS2     | Corretto                          |
-
-
+/// | TC_6.0_1         | VC1                     | Error: Category is not valid      | enum is tested by the enum itself
+/// | TC_6.0_2         | VC2 FD1                 | Error: Invalid date format        | enum is tested by the enum itself
+/// | TC_6.0_3         | VC2 FD2 VD1             | Error: Invalid date               | enum is tested by the enum itself
+/// | TC_6.0_4         | VC2 FD2 VD2 VP1         | Error: Invalid priority           | enum is tested by the enum itself
+/// | TC_6.0_5         | VC2 FD2 VS2 VP2 VS1     | Error: Invalid status             | enum is tested by the enum itself
+/// | TC_6.0_6         | VC2 FD2 VS2 VP2 VS2     | Correct                           |
+/// | TC_6.0_7         | LR1                     | Error: Incorrect search length    |
+/// | TC_6.0_8         | LR2                     | Correct                           |
+/// | TC_6.0_9         | LA1                     | Error: Incorrect city length      |
+/// | TC_6.0_10        | LA2                     | Correct                           |
+/// | TC_6.0_11        | VC2 FD2 VS2 VP2 VS2     | Correct                           |
 
 
 
@@ -31,6 +29,9 @@ void main() {
   _testSearch(description: 'TC_6.0_8', input: 'a'*254, expected: '', reason: 'Il campo search ha lungezza minore di 255 caratteri');
  _testComune(description: 'TC_6.0_9',input: 'a'*300, expected: 'Lunghezza comune deve essere inferiore a 255',reason: 'Il campo comune deve rispettare la lunghezza massima di 255 caratteri');
  _testComune(description: 'TC_6.0_10',input: 'a',expected: null,reason: 'Il campo comune ha lungezza minore di 255 caratteri');
+
+
+ /// test with enums
   /*
   _testForm(description: 'TC_6.0_1', status: StatusReport.accepted , priority:PriorityReport.medium , category: Category.values[8], data: DateTime.now(), expected: 'Errore Categoria non valida', reason: 'Categoria non valida');
  _testForm(description: 'TC_6.0_2', status: StatusReport.accepted, priority: PriorityReport.medium , category: Category.roadDamage, data: DateTime.parse("2022-10-11"), expected: 'Errore Formato data non valido', reason: '');
@@ -38,13 +39,14 @@ void main() {
   _testForm(description: 'TC_6.0_4', status: StatusReport.accepted, priority: PriorityReport.values[10], category: Category.roadDamage, data: DateTime.now(), expected: 'Errore Priorità non valida', reason: 'Priorità non valida');
   _testForm(description: 'TC_6.0_5', status:  StatusReport.values[10], priority: PriorityReport.medium, category: Category.roadDamage, data: DateTime.now(), expected: 'Errore Stato non valido', reason: 'Stato non valido');
   _testForm(description: 'TC_6.0_6', status: StatusReport.accepted, priority: PriorityReport.medium, category: Category.roadDamage, data: DateTime.now(), expected: null, reason: 'Corretto');
-  _testForm(description: 'TC_6.0_7', status: StatusReport.accepted, priority: PriorityReport.medium, category: Category.roadDamage, data: DateTime.now(), expected: null, reason: 'Corretto');
+  _testForm(description: 'TC_6.0_11', status: StatusReport.accepted, priority: PriorityReport.medium, category: Category.roadDamage, data: DateTime.now(), expected: null, reason: 'Corretto');
 _
 */
 
 }
+/// Test for city field
 void _testComune({required String description,required String input,required String? expected,required String reason}) {
-  testWidgets(description, (WidgetTester tester) async {
+  testWidgets(description, (tester) async {
     final key = GlobalKey<FormBuilderFieldState>();
 
     // Renderizza il widget contenente il campo FormBuilderTextField
@@ -75,12 +77,12 @@ void _testComune({required String description,required String input,required Str
 
     // Esegui la validazione del campo
    key.currentState?.validate();
-    String error_text= 'null';
+    //String errorText= 'null';
     if(expected==null){
-      print(description + ":" + "expected:"+" "+error_text + " "+ "reason:" +" "+ reason);
+      //print('$description:expected: $errorText reason: $reason');
     }
     else{
-      print(description + ":" + "expected:"+""+ expected + " "+ "reason:" +" "+ reason);
+      //print('$description:expected:$expected reason: $reason');
     }
 
     // Verifica che l'errore sia quello atteso
@@ -90,8 +92,10 @@ void _testComune({required String description,required String input,required Str
 
   });
 }
+
+/// Test for search field
 void _testSearch({required String description,required String input,required String? expected,required String reason}) {
-  testWidgets(description, (WidgetTester tester) async {
+  testWidgets(description, (tester) async {
     // Crea il widget da testare
 
     String errorText = '';
@@ -142,10 +146,12 @@ void _testSearch({required String description,required String input,required Str
     // Verifica il risultato
     expect(errorText, expected, reason: reason);
 
-    print(description + ":" + "expected:"+" "+ errorText + " "+"reason:" + reason);
+    //print('$description:expected: $errorText reason:$reason');
   });
 }
 
+
+/// Test for all the field of the form
 /*
 void _testForm({
   required String description,
